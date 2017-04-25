4383b84c2840d22be4b1c984d467460aff0f3a54
Luxor <luxor@dookay.com>
2017-04-25 15:30:03 +0800

    新增常用js，首页演示

---
+++ shiatzy-web-mobile/src/main/java/com/dookay/shiatzy/web/mobile/controller/HomeController.java
@@ -0,0 +1,23 @@
+package com.dookay.shiatzy.web.mobile.controller;
+
+import com.dookay.shiatzy.web.mobile.base.MobileBaseController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * @author Luxor
+ * @version v0.0.1
+ * @since 2017/4/25
+ */
+@Controller
+@RequestMapping("")
+public class HomeController extends MobileBaseController {
+
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public ModelAndView index(){
+        ModelAndView mv = new ModelAndView("/home/index");
+        return mv;
+    }
+}
---
+++ shiatzy-web-mobile/src/main/webapp/WEB-INF/views/include/taglib.jsp
@@ -0,0 +1,6 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
+<%@ taglib prefix="fn" uri="http://java.sun.com/jsp/jstl/functions" %>
+<%@ taglib prefix="shiro" uri="http://shiro.apache.org/tags"%>
+<%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
+<c:set var="ctx" value="${pageContext.request.contextPath}"/>
