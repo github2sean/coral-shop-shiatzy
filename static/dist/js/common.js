d4f904f00a700eb66c4e20ebecfba4a52cd0594e
Gloye <gloye.sun@dookay.com>
2017-05-02 09:46:36 +0800

    common.js

--- static/dist/js/common.js
+++ static/dist/js/common.js
@@ -1,44 +1,44 @@
 var commonApp = function () {
-    //返回顶部
-    var backTop = function () {
-        $('#j_back_top').on('click', function (e) {
-            e.preventDefault();
-          $('.main-content').animate({
-            scrollTop: 0
-          }, 400);
-        })
-    };
+  //返回顶部
+  var backTop = function () {
+    $('#j_back_top').on('click', function (e) {
+      e.preventDefault();
+      $('.main-content').animate({
+        scrollTop: 0
+      }, 400);
+    })
+  };
 
   var guideNav = function () {
-        var navPanel = $('#j_nav_panel');
-        //顶部导航显示隐藏
+    var navPanel = $('#j_nav_panel');
+    //顶部导航显示隐藏
     $('#j_show_nav').on("click", function () {
-            navPanel.fadeIn();
+      navPanel.fadeIn();
       $('#j_close_nav').on('click', function () {
-                navPanel.fadeOut();
-            })
-        });
-        //下拉菜单展开收起
+        navPanel.fadeOut();
+      })
+    });
+    //下拉菜单展开收起
     $('.j_drop_down>li').on('click', function () {
-            $(this).toggleClass('active');
-        });
-        //点击栏目后显示图片
+      $(this).toggleClass('active');
+    });
+    //点击栏目后显示图片
     $('.guide-nav>li>a').on('click', function () {
-            $('.search-rec-img').addClass('active');
-        });
-    };
-    //产品列表筛选
+      $('.search-rec-img').addClass('active');
+    });
+  };
+  //产品列表筛选
   var proFilter = function () {
     $('.j_panel_trigger').on('click', function () {
       var id = '#' + $(this).data('panel');
-            $(id).fadeIn();
+      $(id).fadeIn();
       $('.j_close_panel').on('click', function () {
-                $(this).parent().fadeOut();
-            });
-        })
-    };
+        $(this).parent().fadeOut();
+      });
+    })
+  };
 
-    //收藏和预约按钮
+  //收藏和预约按钮
   var collect = function () {
     function clickStyle(btn, originIcon, activeIcon) {
       btn.each(function () {
@@ -47,19 +47,20 @@ var commonApp = function () {
             $(this).removeClass('active').find('use').attr('xlink:href', originIcon);
           } else {
             $(this).addClass('active').find('use').attr('xlink:href', activeIcon);
-                    }
-                });
-            })
-        }
-        var btn = $('.j_collect');
+          }
+        });
+      })
+    }
+
+    var btn = $('.j_collect');
     clickStyle(btn, '#heart', '#heart-red');
 
-        var apbtn = $('.j_appointment');
+    var apbtn = $('.j_appointment');
     clickStyle(apbtn, '#ap-small', '#ap-active');
 
-        var bagbtn = $('.j_bag');
+    var bagbtn = $('.j_bag');
     clickStyle(bagbtn, '#bag', '#bag-active');
-    };
+  };
 
   // 发票信息
   var invoice = function () {
@@ -67,13 +68,13 @@ var commonApp = function () {
       if ($(this).is(":checked")) {
         $(".invoice-info").removeClass("hide")
       }
-    })
+    });
     $("#noInvoice").on("change", function () {
       if ($(this).is(":checked")) {
         $(".invoice-info").addClass("hide")
       }
     })
-  }
+  };
 
   // 支付方式
 
@@ -82,16 +83,16 @@ var commonApp = function () {
       var $this = $(this);
       if ($this.is(":checked")) $this.parents("li").addClass("active").siblings().removeClass("active");
     })
-  }
+  };
 
-    return {
-        init: function () {
-            backTop();
-            guideNav();
-            proFilter();
-            collect();
-          invoice();
-          payment();
-        }
+  return {
+    init: function () {
+      backTop();
+      guideNav();
+      proFilter();
+      collect();
+      invoice();
+      payment();
     }
+  }
 }();
